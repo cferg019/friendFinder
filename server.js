d90611bd4d3237dd2f994e2658abc0596bb3dc59
Casey Ferguson <cferg019@gmail.com>
2020-02-01 20:07:42 -0500

    final push

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 
 var PORT = process.env.PORT || 8080;
 
@@ -6,6 +7,7 @@ var app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'app', 'public')))
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
