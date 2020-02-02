9de0dca66de40394341240fbc10e94985b9bd678
Casey Ferguson <cferg019@gmail.com>
2020-02-01 19:33:04 -0500

    fully functional, need to tweak final file struture

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,47 @@
 var friends = [
     {
-        name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Spongebob Squarepants",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/1200px-SpongeBob_SquarePants_character.svg.png",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     }, {
-        name: "Teddy",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Stimpson J. Cat",
+        photo: "https://vignette.wikia.nocookie.net/nickelodeon/images/e/e8/Stimpy_thinking.png/revision/latest?cb=20190301043127",
         scores: [1, 4, 3, 2, 4, 3, 2, 1, 3, 4]
     }, {
-        name: "Henry",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Helga G. Pataki",
+        photo: "https://vignette.wikia.nocookie.net/p__/images/f/f2/Tumblr_oqrtdqxfH91tr6wqbo1_1280.png/revision/latest?cb=20170530151546&path-prefix=protagonist",
         scores: [3, 4, 2, 3, 2, 1, 4, 3, 1, 1]
+    }, {
+        name: "Eliza Thornberry",
+        photo: "https://vignette.wikia.nocookie.net/nickelodeon/images/9/97/Eliza_Thornberry.png/revision/latest/top-crop/width/360/height/360?cb=20150410060925",
+        scores: [3, 3, 1, 3, 2, 2, 3, 4, 2, 2]
+    }, {
+        name: "Doug Funnie",
+        photo: "https://vignette.wikia.nocookie.net/doug/images/3/3b/Doug001.gif/revision/latest?cb=20110807170511",
+        scores: [3, 4, 3, 1, 1, 2, 1, 2, 5, 5]
+    }, {
+        name: "Angelica Pickles",
+        photo: "https://vignette.wikia.nocookie.net/rugrats/images/1/19/Angelica_Pickles.png/revision/latest/scale-to-width-down/340?cb=20130515182442",
+        scores: [4, 1, 2, 3, 5, 4, 4, 2, 5, 1]
+    }, {
+        name: "Rocko",
+        photo: "https://upload.wikimedia.org/wikipedia/en/e/ee/Rocko_%28Rocko%27s_Modern_Life%29.png",
+        scores: [2, 4, 5, 4, 3, 2, 1, 1, 2, 4]
+    }, {
+        name: "Daria",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/10/Daria_Morgendorffer.png/222px-Daria_Morgendorffer.png",
+        scores: [1, 3, 1, 3, 2, 5, 1, 1, 3, 5]
+    }, {
+        name: "Bart Simpson",
+        photo: "https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png",
+        scores: [2, 3, 3, 4, 4, 1, 5, 4, 1, 5]
+    }, {
+        name: "CatDog",
+        photo: "https://i.pinimg.com/originals/be/1c/24/be1c24fd15d3551dcd17167a71284b61.png",
+        scores: [2, 4, 2, 5, 4, 4, 2, 3, 5, 3]
     }
 ]
 
 
 module.exports = friends;
+
